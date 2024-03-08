5d48ddd22a3d20694276c1bcb93485e509746705
Naman Jain <103953760+namanmodi65@users.noreply.github.com>
2024-03-08 22:25:01 +0530

    prims algo

---
+++ Graphs/prims_algo.cpp
@@ -0,0 +1,37 @@
+int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        vector<int> vis(V,0);
+        vector<pair<int,int>> mst;
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> > q;
+        
+        q.push({0,{0,-1}});
+        int sum = 0;
+        while(!q.empty()){
+            int wt = q.top().first;
+            int node = q.top().second.first;
+            int parent = q.top().second.second;
+            q.pop();
+            if(vis[node] ==1) continue;
+            vis[node] = 1;
+            sum += wt;
+            if(parent == -1){
+                for(auto i:adj[node]){
+                    if(!vis[i[0]])
+                        q.push({i[1],{i[0],node}});
+                }
+            }
+            else{
+                mst.push_back({parent,node});
+                for(auto i:adj[node]){
+                    if(!vis[i[0]])
+                        q.push({i[1],{i[0],node}});
+                }                
+            }
+        }
+        
+        // for(auto i:mst){
+        //     cout<<i.first<<" "<<i.second<<endl;
+        // }
+        
+        return sum;
+    }
