cb2b3af57098a4c6e614151b1703d2b72a222ee7
Naman Jain <103953760+namanmodi65@users.noreply.github.com>
2024-11-04 23:11:22 +0530

    Segment tree Lazy propogation

---
+++ Segment tree/lazy_propogation.cpp
@@ -0,0 +1,145 @@
+//------------------------------------------   es ka matbal m yaha change karke tree convert ho sakta h dusre que ko solve karne ke lea
+
+
+// given an arr and q queies of two type 
+// 1. add x to all valur between l and r 
+// 2. given l r you have to retruen the range minimum
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MOD=1e9+7;
+const int INF=LLONG_MAX>>1;
+int power(int a, int b) {
+    if(b == 0) return 1;
+    a %= MOD;
+    int half_power = power(a, b/2);
+    if(b % 2 == 0) {
+        return half_power * half_power % MOD;
+    } else {
+        return half_power * half_power % MOD * a % MOD;
+    }
+}
+
+vector<int> segTree,lazy;
+
+int merge(int a,int b){
+    return min(a,b);
+}
+
+void build(vector<int> &arr,int start,int end,int idx){//bulid the segment tree
+    // time complexity : o(n)
+    if(start == end){
+        segTree[idx] = arr[start];
+        return;
+    }
+
+    int mid = (start+end)/2;
+    int left = 2*idx;
+    int right = 2*idx+1;
+    build(arr,start,mid,left);
+    build(arr,mid+1,end,right);
+    segTree[idx] = merge(segTree[left],segTree[right]);
+
+}
+
+void propogate(int start,int end,int idx){
+    if(start == end){
+        lazy[idx] = 0;
+        return ;
+    } //leaf node
+    lazy[2*idx] += lazy[idx];
+    lazy[2*idx+1] += lazy[idx];
+    lazy[idx] = 0;
+}
+
+void update(int start,int end,int idx,int l,int r,int val){//update at a given point in segment tree
+    // time complexity : o(logn)
+    
+    if(lazy[idx] != 0){
+        segTree[idx] += lazy[idx];//-----------------------------------
+        propogate(start,end,idx);
+    }
+
+    if(start>=l && end<=r){////complete overlap
+        segTree[idx] += val;//--------------------------------
+        lazy[idx] += val;
+        propogate(start,end,idx);
+        return;
+    }
+
+    if(start>r || end<l) return ; //disjoin
+    int mid = (start+end)/2;
+    update(start,mid,2*idx,l,r,val);
+    update(mid+1,end,2*idx+1,l,r,val);
+
+    segTree[idx] = merge(segTree[2*idx],segTree[2*idx+1]);
+}
+
+int query(int start,int end,int idx,int l,int r){//give sum L to R
+    // time complexity : o(logn)
+
+    if(lazy[idx] != 0){
+        segTree[idx] += lazy[idx];//-----------------------------------
+        propogate(start,end,idx);
+    }
+
+    //complete overlap
+    if(start>=l && end<=r) return segTree[idx];
+    //disJoin
+    if(l > end || r < start) return INT_MAX;//----------------------------------
+    int mid = (start+end)/2;
+    int leftAns = query(start,mid,2*idx,l,r);
+    int rightAns = query(mid+1,end,2*idx+1,l,r);
+    return merge(leftAns,rightAns);
+}
+
+void solve(){
+    int n;cin>>n;
+    vector<int> arr(n);
+    segTree.resize(4*n);
+    lazy.resize(4*n);
+    for(auto &i:arr) cin>>i;
+    build(arr,0,n-1,1);
+
+    for(auto i:arr){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    for(auto i:segTree){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    for(auto i:lazy){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    cout<<endl;
+    cout<<query(0,n-1,1,0,2)<<endl;
+    update(0,n-1,1,0,0,500);
+    cout<<query(0,n-1,1,0,2)<<endl;
+    for(auto i:segTree){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    for(auto i:lazy){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    cout<<endl;
+
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    
+   return 0;
+}
